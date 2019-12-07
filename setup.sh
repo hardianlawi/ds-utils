9f52776a7c6a000ac78575a81aa64572c334432e
hardianlawi <hardianlawi@gmail.com>
2019-12-07 21:03:48 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-sudo apt update && sudo apt install -y git g++ make zsh rake wget redis-server libspatialindex-dev
+sudo apt update && \
+	sudo apt install -y git g++ make zsh rake wget redis-server libspatialindex-dev \
+		gcc htop build-essential tmux cmake libboost-dev libboost-program-options-dev \
+		libboost-system-dev libboost-thread-dev libboost-math-dev libboost-test-dev \
+		libboost-python-dev zlib1g-dev
 
 # Change bash to zsh
 sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
@@ -40,7 +44,17 @@ cd autojump && ./install.py && cd && rm -rf autojump
 sed -i 's/plugins=(git)/plugins=(autojump git history z zsh-autosuggestions kubectl)/g' .zshrc
 
 # Add LC_ALL and LANG
+# sudo locale-gen en_SG.UTF-8
 printf "\nexport LC_ALL=C.UTF-8" >> .zshrc
 printf "\nexport LANG=C.UTF-8" >> .zshrc
 
 source .zshrc
+
+# Setup jupyter notebook
+jupyter notebook --generate-config
+
+pip install jupyter_contrib_nbextensions ipywidgets jupyterthemes && 
+	jupyter contrib nbextension install --user && \
+	jupyter nbextension enable --py widgetsnbextension
+
+jt -t grade3 -fs 95 -tfs 11 -nfs 115 -cellw 88% -kl -N -T
