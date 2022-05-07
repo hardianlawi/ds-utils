f9335c5418701ae51183cd0d9e818243abd1813d
hardianlawi <hardianlawi@gmail.com>
2022-05-07 09:12:46 +0000

    Add new import

--- ds_utils/common_imports.py
+++ ds_utils/common_imports.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import torch
+from IPython.display import display
 
 from ds_utils.data_utils import UniversalStore
 from ds_utils.random import seed_everything
