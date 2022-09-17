4c02e644612762a1abb0085c1dd2cbf3e3385929
Hardian Lawi <hardianlawi@gmail.com>
2022-09-17 13:44:19 +0800

    Update data_utils.py

--- ds_utils/data_utils.py
+++ ds_utils/data_utils.py
@@ -9,30 +9,23 @@ import pandas as pd
 class Store:
     def __init__(self, root_dir):
         self._root_dir = root_dir
-        if not self._root_dir.startswith("s3://"):
-            os.makedirs(self._root_dir, exist_ok=True)
+        os.makedirs(self._root_dir, exist_ok=True)
+
+    def __setitem__(self, key: str, value: pd.DataFrame):
+        if not isinstance(value, pd.DataFrame):
+            raise TypeError(f"value should be pd.DataFrame, but found {type(value)}")
 
 
-class CSVStore(Store):
+class CsvStore(Store):
     def __init__(self, root_dir):
-        super(CSVStore, self).__init__(root_dir)
+        super(CsvStore, self).__init__(root_dir)
 
     def __getitem__(self, key: str):
         return pd.read_csv(join(self._root_dir, key) + ".csv")
 
     def __setitem__(self, key: str, value: pd.DataFrame):
-        value.to_feather(join(self._root_dir, key) + ".feather")
-
-
-class TSVStore(Store):
-    def __init__(self, root_dir):
-        super(TSVStore, self).__init__(root_dir)
-
-    def __getitem__(self, key: str):
-        return pd.read_csv(join(self._root_dir, key) + ".tsv", sep="\t")
-
-    def __setitem__(self, key: str, value: pd.DataFrame):
-        value.to_feather(join(self._root_dir, key) + ".feather")
+        super().__setitem__(key, value)
+        value.to_csv(join(self._root_dir, key) + ".csv", index=False)
 
 
 class FeatherStore(Store):
@@ -43,6 +36,7 @@ class FeatherStore(Store):
         return pd.read_feather(join(self._root_dir, key) + ".feather")
 
     def __setitem__(self, key: str, value: pd.DataFrame):
+        super().__setitem__(key, value)
         value.to_feather(join(self._root_dir, key) + ".feather")
 
 
@@ -58,57 +52,96 @@ class PickleStore(Store):
             pickle.dump(value, f)
 
 
-class JSONStore(Store):
+class DocBinStore(PickleStore):
     def __init__(self, root_dir):
-        super(JSONStore, self).__init__(root_dir)
+        super(DocBinStore, self).__init__(root_dir)
+
+    @profile
+    def __getitem__(self, key: str):
+        doc_bin_bytes = super().__getitem__(key)
+        return DocBin().from_bytes(doc_bin_bytes)
+
+    def __setitem__(self, key: str, docs):
+        doc_bin = DocBin(store_user_data=False)
+        for doc in docs:
+            doc_bin.add(doc)
+        doc_bin_bytes = doc_bin.to_bytes()
+        super().__setitem__(key, doc_bin_bytes)
+
+
+class JsonStore(Store):
+    def __init__(self, root_dir):
+        super(JsonStore, self).__init__(root_dir)
 
     def __getitem__(self, key: str):
-        return pd.read_json(join(self._root_dir, key) + ".json")
+        with open(join(self._root_dir, key) + ".json", "r") as f:
+            return json.load(f)
 
     def __setitem__(self, key: str, value):
-        with open(join(self._root_dir, key) + ".json", "wb") as f:
-            pickle.dump(value, f)
+        with open(join(self._root_dir, key) + ".json", "w") as f:
+            json.dump(value, f)
 
 
-class UniversalStore(CSVStore, FeatherStore, PickleStore):
+class JsonlStore(Store):
+    def __init__(self, root_dir):
+        super(JsonlStore, self).__init__(root_dir)
+
+    def __getitem__(self, key: str):
+        return pd.read_json(join(self._root_dir, key) + ".jsonl", lines=True)
+
+    def __setitem__(self, key: str, value):
+        with open(join(self._root_dir, key) + ".jsonl", "w") as f:
+            for line in value:
+                f.write(json.dumps(line) + "\n")
+
+
+class UniversalStore(FeatherStore, PickleStore):
     def __init__(self, root_dir):
         FeatherStore.__init__(self, root_dir)
-        self._supported_exts = {
-            ".csv": CSVStore,
+        self._supported_fmts = {
             ".feather": FeatherStore,
+            ".csv": CsvStore,
             ".pkl": PickleStore,
-            ".tsv": TSVStore,
-            ".json": JSONStore,
+            ".json": JsonStore,
+            ".jsonl": JsonlStore,
         }
 
     def __getitem__(self, key: str):
-        fmt = self._get_format(key)
-        if len(fmt) == 0:
-            path = join(self._root_dir, key)
-            paths = self._get_possible_paths(path)
-            if len(paths) == 0:
-                raise Exception(f"{path} does not exist.")
-            if len(paths) > 1:
-                raise Exception(f"There are two files with the same base name {paths}.")
-            path = paths[0]
-            return self._supported_exts[fmt].__getitem__(self, key)
-        elif fmt not in self._supported_exts:
+        path = join(self._root_dir, key)
+        paths = self._get_possible_paths(path)
+        if len(paths) == 0:
+            raise Exception(f"{path} does not exist.")
+        if len(paths) > 1:
+            raise Exception(f"There are two files with the same base name {paths}.")
+        fmt = self._get_format(paths[0])
+        if fmt not in self._supported_fmts:
             raise Exception(f"Format {fmt} is not recognized")
-        else:
-            return self._supported_exts[fmt].__getitem__(self, key[: -len(fmt)])
-
-    def _get_possible_paths(self, path):
-        return [
-            path + ext
-            for ext in self._supported_exts
-            if pathlib.Path(path + ext).exists()
-        ]
+        key = self.__path_with_no_fmt(key, fmt)
+        return self._supported_fmts[fmt].__getitem__(self, key)
 
     def __setitem__(self, key: str, value):
         fmt = self._get_format(key)
-        if fmt not in self._supported_exts:
+        if fmt not in self._supported_fmts:
             raise Exception(f"Format {fmt} is not recognized")
-        self._supported_exts[fmt].__setitem__(self, key.replace(fmt, ""), value)
+        key = self.__path_with_no_fmt(key, fmt)
+        self._supported_fmts[fmt].__setitem__(self, key, value)
+
+    def _get_possible_paths(self, path):
+        return [
+            self.__path_with_fmt(path, fmt)
+            for fmt in self._supported_fmts
+            if pathlib.Path(self.__path_with_fmt(path, fmt)).exists()
+        ]
+
+    def __path_with_fmt(self, path, fmt):
+        if path.endswith(fmt):
+            return path
+        return path + fmt
+
+    def __path_with_no_fmt(self, path, fmt):
+        if path.endswith(fmt):
+            return path[: -len(fmt)]
+        return path
 
     def _get_format(self, path):
         return pathlib.Path(path).suffix
